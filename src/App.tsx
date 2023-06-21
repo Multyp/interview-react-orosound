093e475330297cac0f733f76f1c94e6bb7c78d30
Miguel Da Silva <108199883+Multyp@users.noreply.github.com>
2023-06-21 16:23:52 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/container_items/Header"
+
+import "./App.css"
+
+function App() {
+
+  return (
+      <div className='container'>
+        <Header/>
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/container_items/Header.tsx
@@ -0,0 +1,14 @@
+import "./Style/Header.css"
+
+function Header() {
+
+    return (
+      <>
+        <div className='header-banner'>
+            <h1>Users managing</h1>
+        </div>
+      </>
+    )
+}
+
+export default Header;
